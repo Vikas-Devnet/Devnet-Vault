6e56259d8f9a6335c5f883d155683168261805aa
vikas4240 <vikas.kumar@rosmertatech.co.in>
2026-01-17 10:49:05 +0530

    Add project files.

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        // Bind appsettings section -> JwtSettings
+
+        // Register Jwt service
+
+        return services;
+    }
+}
